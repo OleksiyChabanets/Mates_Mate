757fea79b40e145ef6dbbc056350d90aad17a157
Chabanets Oleksiy <chabanetsan@gmail.com>
2023-01-08 16:04:50 +0300

    Added multy function

--- capt_mate_app/beginning.py
+++ capt_mate_app/beginning.py
@@ -4,8 +4,11 @@ def plus(a,b):
 def minus(a,b):
     return a-b
 
+def multy(a,b):
+    return a*b
+
 
 if __name__ == '__main__':
     print(plus(2,2))
     print(minus(7,2))
-
+    print(multy(3,4))
